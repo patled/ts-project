4534126497344fd63b54b1b4f915ce206aa5a01e
patled <github@lederer.cloud>
2023-11-25 07:18:36 +0100

    Fix HTML and TypeScript formatting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-console.log("Hello, World!1");
+console.log('Hello, World!1');
 
-const h1 = document.createElement("h1");
-h1.textContent = "Hello, World!";
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello, World!';
 document.body.appendChild(h1);
