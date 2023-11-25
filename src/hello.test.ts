ec5b9199579ed257c4504d0494f82c46b40d57da
patled <github@lederer.cloud>
2023-11-25 07:39:47 +0100

    Add test for Hello class

---
+++ src/hello.test.ts
@@ -0,0 +1,19 @@
+import { Hello } from './hello';
+
+describe('Hello class', () => {
+  let hello: Hello;
+
+  beforeEach(() => {
+    hello = new Hello();
+  });
+
+  it('should add header', () => {
+    const text = 'Hello, World!';
+    hello.addHeader(text);
+
+    // test it the tett is a h1
+    const h1 = document.querySelector('h1');
+    expect(h1).toBeTruthy();
+    expect(h1!.textContent).toBe(text);
+  });
+}); 
